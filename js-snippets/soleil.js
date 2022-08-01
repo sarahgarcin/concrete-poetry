51f89b04e3dc877cefb366af2e4994653040e7f8
Sarah Garcin <contact@sarahgarcin.com>
2022-08-01 12:35:55 +0200

    test soleil function level up

--- js-snippets/soleil.js
+++ js-snippets/soleil.js
@@ -1,4 +1,6 @@
-  // fonction qui permet d'écrire un texte en répartissant les lettres en soleil
+let soleilClickCounter = 10;
+
+// fonction qui permet d'écrire un texte en répartissant les lettres en soleil
 function activateSoleil(){
   soleilMode = true;
 
@@ -16,6 +18,14 @@ function activateSoleil(){
 
   $('.gui-group.change-size').css('display', 'none');
   $('.gui-group.change-case').css('display', 'block');
+
+  if(soleilClickCounter > 3){
+    soleilClickCounter --;
+  }
+  else{
+    soleilClickCounter = 10;
+  }
+
 }
 
 function soleil(){
@@ -28,12 +38,20 @@ function soleil(){
     originalText = originalText.toLowerCase();
   }
 
-  var r     = 30; //taille du rayon
+  // var r     = 30; //taille du rayon
+  // var nbOfRepet = 8; // nombre de lettres par cercle
+
+  // var r     = 30; //taille du rayon
+  // var nbOfRepet = 8; // nombre de lettres par cercle
+  var r     = soleilClickCounter * 4; //taille du rayon
+  var nbOfRepet = soleilClickCounter; // nombre de lettres par cercle
+
   var angle = 0;
-  var nbOfRepet = 8; // nombre de lettres par cercle
   var step = TWO_PI/nbOfRepet; //in radians equivalent of 360/6 in
+  var n = 0; 
   
   var chars = originalText.split('');
+  chars = chars.filter(e => String(e).trim());
   // calculer la taille de la typo
   let fontSize = 0.08*sqrt((width * height)/chars.length);
 
@@ -56,20 +74,47 @@ function soleil(){
           var y = r * cos(angle);
           poster.text(chars[i], x, y);
           //increase angle by step size
-          angle = angle + step;
+          angle = angle + step; 
       }
       // increase size of circle
-      r = r + 40;
+      r = r + 40; 
     }
     poster.pop();
   }
   else{
-    poster.textSize(25);
     poster.push();
-    poster.translate(State.textX + width/2, State.textY);
-    poster.text('Erreur, ce mode ne fonctionne qu\'avec 8 lettres maximum', 0, 0, pageW);
+    poster.translate(State.textX + width/2, State.textY + height/2);
+    poster.textSize(20);
+    // affiche la première lettre au milieu
+    poster.text(chars[0], 0, 0);
+    r = r + 20;
+    for(var i = 1; i<chars.length; i++){
+      // for(var n = 0; n<nbOfRepet; n++){
+          //convert polar coordinates to cartesian coordinates
+          var x = r * sin(angle);
+          var y = r * cos(angle);
+          poster.text(chars[i], x, y);
+          //increase angle by step size
+          angle = angle + step;
+          n ++;
+      // }
+      // increase size of circle
+      if(n == nbOfRepet){
+        r = r + 40;
+        n = 0;
+      }
+      
+    }
     poster.pop();
+
   }
+  // else{
+  //   poster.textSize(25);
+  //   poster.push();
+  //   poster.translate(State.textX + width/2, State.textY);
+  //   poster.text('Erreur, ce mode ne fonctionne qu\'avec 8 lettres maximum', 0, 0, pageW);
+  //   poster.pop();
+  // }
 
   
 }
